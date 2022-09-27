960865326cbec946a893469704a7d020ca1680f2
Visha Arumugam <arseethai@gmail.com>
2022-09-27 00:15:08 -0500

    Web application Commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from crypt import methods
 import pickle
 from flask import Flask,request,jsonify,app,url_for,render_template
 import numpy as np
@@ -15,7 +14,7 @@ def home():
 
 @app.route('/predict_api',methods=['POST'])
 
-def predict_add():
+def predict_api():
     data= request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -23,6 +22,15 @@ def predict_add():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The house preice Prediction is {}".format(output))
+
 
 if __name__=="__main__":
     app.run(debug=True)
